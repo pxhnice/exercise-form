03d09361bcabfbb99355e841136eef6c96ec59d8
小白菜 <568099849@qq.com>
2023-12-27 11:31:33 +0800

    docs: 添加测试组件

---
+++ docs/.vitepress/vitepress/components/vp-app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Custom Layout!</h1>
+    <Content /><!-- make sure to include markdown outlet -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ docs/.vitepress/vitepress/index.ts
@@ -0,0 +1,4 @@
+import VpApp from "./components/vp-app.vue";
+import "./styles/index.scss";
+
+export default VpApp;

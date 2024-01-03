5a32cd8674de82b5c5dd54e3afa8c4a69d0a7631
小白菜 <568099849@qq.com>
2024-01-03 15:32:50 +0800

    fix: 类型处理

--- packages/utils/icons.ts
+++ packages/utils/icons.ts
@@ -1,6 +1,8 @@
+import type { App } from "vue";
+
 export const modules = import.meta.glob("../icons/svg/*.svg", { eager: true });
 
-export default function install(app: any) {
+export default function install(app: App) {
   for (const [key, value] of Object.entries(modules)) {
     const _name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
     let _value = value as any;
--- packages/widget/designer/fields/index.ts
+++ packages/widget/designer/fields/index.ts
@@ -1,6 +1,8 @@
+import type { App } from "vue";
+
 const components = import.meta.glob("./*.vue", { eager: true });
 
-export default function install(app: any) {
+export default function install(app: App) {
   for (const [key, value] of Object.entries(components)) {
     const _name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
     let _value = value as any;

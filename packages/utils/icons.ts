31f53ca5c4b51a5cc1da2e065041108850ff6374
小白菜 <568099849@qq.com>
2024-01-03 11:58:15 +0800

    fix: 组件批量注册由异步转为同步

--- packages/utils/icons.ts
+++ packages/utils/icons.ts
@@ -1,12 +1,9 @@
-import { App, defineAsyncComponent } from 'vue';
+export const modules = import.meta.glob("../icons/svg/*.svg", { eager: true });
 
-import type { AsyncComponentLoader } from "vue";
-
-export const modules = import.meta.glob("../icons/svg/*.svg");
-
-export default function install(app: App) {
+export default function install(app: any) {
   for (const [key, value] of Object.entries(modules)) {
-    const name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
-    app.component(`ex-icon-${name}`, defineAsyncComponent(value as AsyncComponentLoader));
+    const _name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
+    let _value = value as any;
+    app.component(`ex-icon-${_name}`, _value.default);
   }
 }
--- packages/widget/designer/property/index.ts
+++ packages/widget/designer/property/index.ts
@@ -1,11 +1,9 @@
-import { defineAsyncComponent } from "vue";
-import type { AsyncComponentLoader } from "vue";
-const components = import.meta.glob("./**/*.vue");
+const components = import.meta.glob("./*.vue", { eager: true });
 
 export default function install(app: any) {
-  // 遍历对象并注册异步组件
   for (const [key, value] of Object.entries(components)) {
-    const name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
-    app.component(name, defineAsyncComponent(value as AsyncComponentLoader));
+    const _name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
+    let _value = value as any;
+    app.component(_name, _value.default);
   }
 }

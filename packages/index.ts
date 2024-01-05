02ded151b17ec1b7f4c4fcc3217e67ed2a13ce7e
小白菜 <568099849@qq.com>
2024-01-05 11:28:57 +0800

    fix: 修复组件导出无提示

--- packages/index.ts
+++ packages/index.ts
@@ -1,8 +1,9 @@
-export * from "@exercise-form/components";
+export * from "@exercise-form/components/index";
 export * from "@exercise-form/constants";
 export * from "@exercise-form/utils";
 
 import installer from "./defaults";
 
 export const install = installer.install;
+export const version = installer.version;
 export default installer;

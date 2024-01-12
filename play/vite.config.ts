282cb7c3b1cab90e4bb1077f0fa242fe68e86da5
小白菜 <568099849@qq.com>
2024-01-12 18:01:42 +0800

    fix: 本地测试访问更换

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,6 +1,7 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), svgLoader()]
 });

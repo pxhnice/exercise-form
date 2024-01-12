1ae93f66ba6e3ac63b0d20e122557dae8f63d587
小白菜 <568099849@qq.com>
2024-01-12 18:01:42 +0800

    fix: 本地测试访问更换

--- play/src/main.ts
+++ play/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import ExForm from "../../packages/exercise-form";
+import ExForm from "../../packages";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import "element-plus/theme-chalk/dark/css-vars.css";
@@ -13,7 +13,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-// app.use(installIcon);
 app.use(ElementPlus, { locale: zhCn, size: "default" });
 app.use(ExForm as any);
 app.mount("#app");

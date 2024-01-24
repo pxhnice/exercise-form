10d8ea44bcd24a59e19763c54ae7a78b2656870e
小白菜 <568099849@qq.com>
2024-01-24 18:14:18 +0800

    fix: 构建器按钮配置处理

--- packages/components/form-designer/src/form-designer.ts
+++ packages/components/form-designer/src/form-designer.ts
@@ -50,18 +50,7 @@ export const formDesignerProps = {
   options: {
     type: definePropType<DesOptions>(Object),
     default() {
-      return {
-        templateButton: true, //是否显示模版栏
-        treeFormButton: true, //是否显示预览表单按钮
-        previewFormButton: true, //是否显示预览表单按钮
-        exportJsonButton: true, //是否显示导出JSON器按钮
-        exportCodeButton: true, //是否显示导出代码按钮
-        generateSFCButton: true, //是否显示生成SFC按钮
-        pageTypeButton: true, //是否显示导出页面类型按钮
-        copyDataButton: true, //是否显示复制按钮
-        saveFileButton: true, //是否显示导出文件按钮
-        resetFormJson: false //是否初始化重置表单为空
-      };
+      return {};
     }
   },
   /**

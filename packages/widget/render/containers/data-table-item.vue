a0b7b625194c9221e0951affb9aec304bd834201
pxhgood <568099849@qq.com>
2024-01-28 16:38:02 +0800

    feat: 新增数据表格树形预览渲染组件

---
+++ packages/widget/render/containers/data-table-item.vue
@@ -0,0 +1,96 @@
+<template>
+  <div style="padding: 2px">
+    <el-table
+      :style="{ width: widgetData.options.tableWidth }"
+      :data="widgetData.options.tableData"
+      v-bind="widgetData.options"
+      class="ex-data-table"
+    >
+      <el-table-column
+        v-if="widgetData.options.showIndex"
+        label="序号"
+        width="60"
+        align="center"
+        type="index"
+      />
+      <el-table-column
+        v-if="widgetData.options.showCheckBox"
+        width="60"
+        align="center"
+        type="selection"
+      />
+      <template v-for="cols in widgetData.options.tableColumns" :key="cols">
+        <el-table-column
+          v-if="cols.type === 'expand'"
+          :type="cols.type"
+          v-bind="cols"
+          v-slot="scope"
+        >
+          <component
+            v-if="cols.render"
+            :is="cols.render"
+            :row="scope.row"
+            :index="scope.$index"
+          />
+          <slot
+            v-else
+            :row="scope.row"
+            name="expand"
+            :index="scope.$index"
+          ></slot>
+        </el-table-column>
+        <data-table-column-widget
+          v-else-if="!cols.type && cols.show"
+          :cols="cols"
+        >
+          <template v-for="slot in Object.keys($slots)" #[slot]="scope">
+            <slot :name="slot" v-bind="scope"></slot>
+          </template>
+        </data-table-column-widget>
+      </template>
+      <el-table-column
+        v-if="widgetData.options.showOperation"
+        :align="widgetData.options.operationAlign"
+        :label="widgetData.options.operationLabel"
+        :width="widgetData.options.operationWidth"
+        :fixed="
+          widgetData.options.operationFixed != ''
+            ? widgetData.options.operationFixed
+            : undefined
+        "
+      >
+        <template #default>
+          <el-button
+            v-for="(btn, index) of widgetData.options.operationButtons"
+            :key="index"
+            v-bind="btn"
+          >
+            {{ btn.label }}
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      v-if="widgetData.options.showPagination"
+      :small="widgetData.options.small"
+      :class="paginationAlign"
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { renContainerProps } from "./container";
+
+const props = defineProps(renContainerProps);
+
+const paginationAlign = computed(() => {
+  let align = props.widgetData.options.paginationAlign;
+  if (align == "left") return "ex-pagination-left";
+  if (align == "right") return "ex-pagination-right";
+  return "ex-pagination-center";
+});
+</script>
---
+++ packages/widget/render/containers/data-tree-item.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="ex-data-tree">
+    <el-input
+      v-if="widgetData.options.showFilter"
+      v-model="treeValue"
+      prefix-icon="Search"
+      clearable
+    />
+    <div>
+      <el-checkbox
+        v-if="widgetData.options.showExpandOrRetract"
+        v-model="checkboxValue"
+        @change="handleExpandOrRetract"
+        label="展开/折叠"
+      />
+      <el-checkbox
+        v-if="widgetData.options.showCheckAllOrCancelAll"
+        v-model="expandAllValue"
+        @change="handleSelectAll"
+        label="全选/全不选"
+      />
+      <el-checkbox
+        v-if="widgetData.options.showLinkage"
+        v-model="checkStrictly"
+        label="父子联动"
+      />
+    </div>
+    <el-tree
+      ref="treeRef"
+      :check-strictly="!checkStrictly"
+      :data="treeData"
+      v-bind="widgetData.options"
+      :filter-node-method="filterNode"
+    >
+      <template #default="{ node }">
+        <div>{{ node.label }}</div>
+        <div
+          class="ex-tree-buttons"
+          v-if="widgetData.options.showOperationButton"
+        >
+          <el-button type="primary" link>添加</el-button>
+          <el-button type="primary" link>删除</el-button>
+        </div>
+      </template>
+    </el-tree>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import { renContainerProps } from "./container";
+
+interface TreeData {
+  id: number;
+  label: string;
+  children?: TreeData[];
+}
+
+const props = defineProps(renContainerProps);
+
+const treeData = props.widgetData.options.treeData as TreeData[];
+const treeValue = ref("");
+const checkboxValue = ref(true);
+const expandAllValue = ref(false);
+const checkStrictly = ref(true);
+
+const treeRef = ref();
+
+watch(treeValue, (val) => {
+  treeRef.value!.filter(val);
+});
+
+const flatColumn = (columns: TreeData[], flatArr: Array<number> = []) => {
+  columns.forEach((col) => {
+    flatArr.push(col.id);
+    if (col.children?.length) {
+      flatArr.push(...flatColumn(col.children));
+    }
+  });
+  return flatArr;
+};
+
+const handleExpandOrRetract = (value: boolean) => {
+  let nodesMap = treeRef.value!.store.nodesMap;
+  for (let key in nodesMap) {
+    nodesMap[key].expanded = value;
+  }
+};
+
+const handleSelectAll = (value: boolean) => {
+  if (value) {
+    let ids = flatColumn(treeData);
+    treeRef.value!.setCheckedKeys(ids);
+  } else {
+    treeRef.value!.setCheckedNodes([]);
+  }
+};
+
+const filterNode = (value: string, data: TreeData) => {
+  if (!value) return true;
+  return data.label.includes(value);
+};
+</script>

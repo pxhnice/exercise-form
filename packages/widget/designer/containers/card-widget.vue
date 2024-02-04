6a870707c4955243c14aa817b4d9e038796cc1ae
小白菜 <568099849@qq.com>
2024-02-04 15:14:00 +0800

    fix: 样式调整

--- packages/widget/designer/containers/card-widget.vue
+++ packages/widget/designer/containers/card-widget.vue
@@ -6,59 +6,60 @@
     :widget-sub="widgetSub"
     :designer="designer"
   >
-    <el-card
-      class="ex-widget-card"
-      :style="{ width: widgetData.options.cardWidth }"
-      v-bind="widgetData.options"
-      @click.stop="onClickTabs"
-    >
-      <template #header>
-        <div class="ex-widget-card_header">
-          <span>{{ widgetData.options.label }}</span>
+    <div class="ex-widget-card">
+      <el-card
+        :style="{ width: widgetData.options.cardWidth }"
+        v-bind="widgetData.options"
+        @click.stop="onClickTabs"
+      >
+        <template #header>
+          <div class="ex-widget-card_header">
+            <span>{{ widgetData.options.label }}</span>
+          </div>
+        </template>
+        <div style="display: flex">
+          <div class="ex-widget-card_body">
+            <draggable
+              group="componentsGroup"
+              ghost-class="ex-ghost"
+              handle=".drag-mover"
+              class="container-draggable"
+              :disabled="isDrag"
+              :list="widgetData.children"
+              item-key="id"
+              :animation="300"
+              @add="onDragAdd($event, widgetData)"
+            >
+              <template #item="{ element, index }">
+                <div class="drag-container">
+                  <template v-if="element.category === 'container'">
+                    <component
+                      :is="`${element.type}-widget`"
+                      :widget-data="element"
+                      :parent-data="widgetData"
+                      :designer="designer"
+                      :parent-list="widgetData.children"
+                      :widget-sub="index"
+                      :key="element.id"
+                    />
+                  </template>
+                  <template v-else>
+                    <form-item-widget
+                      :designer="designer"
+                      :widget-data="element"
+                      :parent-data="widgetData"
+                      :parent-list="widgetData.children"
+                      :widget-sub="index"
+                      :key="element.id"
+                    />
+                  </template>
+                </div>
+              </template>
+            </draggable>
+          </div>
         </div>
-      </template>
-      <div style="display: flex">
-        <div class="ex-widget-card_body">
-          <draggable
-            group="componentsGroup"
-            ghost-class="ex-ghost"
-            handle=".drag-mover"
-            class="container-draggable"
-            :disabled="isDrag"
-            :list="widgetData.children"
-            item-key="id"
-            :animation="300"
-            @add="onDragAdd($event, widgetData)"
-          >
-            <template #item="{ element, index }">
-              <div class="drag-container">
-                <template v-if="element.category === 'container'">
-                  <component
-                    :is="`${element.type}-widget`"
-                    :widget-data="element"
-                    :parent-data="widgetData"
-                    :designer="designer"
-                    :parent-list="widgetData.children"
-                    :widget-sub="index"
-                    :key="element.id"
-                  />
-                </template>
-                <template v-else>
-                  <form-item-widget
-                    :designer="designer"
-                    :widget-data="element"
-                    :parent-data="widgetData"
-                    :parent-list="widgetData.children"
-                    :widget-sub="index"
-                    :key="element.id"
-                  />
-                </template>
-              </div>
-            </template>
-          </draggable>
-        </div>
-      </div>
-    </el-card>
+      </el-card>
+    </div>
   </container-wrapper>
 </template>
 

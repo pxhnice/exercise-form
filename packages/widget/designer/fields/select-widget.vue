4492855c81bd0f6280c4207df2b99b076939a881
小白菜 <568099849@qq.com>
2024-02-06 10:57:03 +0800

    fix: 修复下拉选单选多选切换报错问题

--- packages/widget/designer/fields/select-widget.vue
+++ packages/widget/designer/fields/select-widget.vue
@@ -4,10 +4,12 @@
     :required="widgetData.options.required"
     :label-width="widgetData.options.labelWidth"
   >
+    {{ selectValue }}
     <el-select
       v-model="selectValue"
       v-bind="widgetData.options"
       :teleported="false"
+      :key="selectKey"
     >
       <el-option
         v-for="item in options"
@@ -20,10 +22,20 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import { desFieldsProps } from "./fields";
 import { useOptions } from "@exercise-form/hooks";
 
 const props = defineProps(desFieldsProps);
 
+const selectKey = ref(props.widgetData.id);
+
 const { selectValue, options, label, value } = useOptions(props.widgetData);
+
+watch(
+  () => props.widgetData.options.multiple,
+  () => {
+    selectKey.value = selectKey.value + Math.random();
+  }
+);
 </script>
